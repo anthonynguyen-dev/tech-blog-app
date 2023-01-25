464db30a9f2ed21076849a1438f7b75dea97360c
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-01-25 17:21:34 -0600

    added to index.js models

--- models/index.js
+++ models/index.js
@@ -1,27 +1,13 @@
-const { Model, Datatypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const User = require("./User");
+const Blog = require("./Blog");
+const Comment = require("./Comment");
 
-class  extends Model {}
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
+});
 
-Blog.init(
-  {
-    id: {
-      type: Datatypes.INTERGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: Datatypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "blog",
-  }
-);
+Comment.belongsTo(Blog, {
+  foreignKey: "gallery_id",
+});
 
-module.exports = Gallery;
+module.exports = { User, Blog, Comment };
