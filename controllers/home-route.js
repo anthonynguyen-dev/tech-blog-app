9d9199bfd2c58252d6ccd87356b336314bbceb62
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-01-30 17:08:46 -0600

    added home route

---
+++ controllers/home-route.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const { User, Blog } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
