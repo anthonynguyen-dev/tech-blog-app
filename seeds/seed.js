a925d347548cdaa12a3bc71c8e2894d0c0e9a116
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-01-30 02:44:19 -0600

    added to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,3 +2,18 @@ const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
 
 const userData = require("./userSeed.json");
+
+const seedDB = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({});
+  }
+  process.exit(0);
+};
+
+seedDB();
